9df1f0f56d1cffb74af1b0752aafb0814b6155fd
Austin Berry <aberry52098@gmail.com>
2023-09-26 16:46:05 -0400

    add list

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,30 @@
 import { useState } from 'react'
 
 export function Form() {
-  
+  const [list, setList] = useState([]);
+  const [value, setValue] = useState("");
+
+  function addItem() {
+    if (value !== "") {
+      setList([...list, value]);
+      setValue("");
+    }
+  };
+  function handleChange(event) {
+    setValue(event.target.value);
+  };
+  console.log(list);
   return (
     <>
       <form>
-        <input type='text' placeholder='add item'/>
-        <button type='button'>Submit</button>
-        <p>testing</p>
+        <input type='text' placeholder='add item' value={value} onChange={(event) => { handleChange(event) }} />
+        <button type='button' onClick={addItem}>Add Item</button>
       </form>
+      <ul>
+        {list.map((item, index) =>
+          <li key={index}>{item}</li>
+        )}
+      </ul>
     </>
   )
 }
