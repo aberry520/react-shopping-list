a917cb04d6c82da2070d005b57b96fe149c5ec45
Austin Berry <aberry52098@gmail.com>
2023-09-26 14:06:07 -0400

    Change list to form

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+export function Form() {
+  
+  return (
+    <>
+      <form>
+        <input type='text' placeholder='add item'/>
+        <button type='button'>Submit</button>
+        <p>testing</p>
+      </form>
+    </>
+  )
+}
