cbf830adb7d6b2e17cba0fd0085eaa5115d76450
Austin Berry <aberry52098@gmail.com>
2023-09-26 17:01:14 -0400

    add clear button that runs clearList function

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,21 +4,26 @@ export function Form() {
   const [list, setList] = useState([]);
   const [value, setValue] = useState("");
 
-  function addItem() {
+  const addItem = ()=>{
     if (value !== "") {
       setList([...list, value]);
       setValue("");
     }
   };
-  function handleChange(event) {
+  const handleChange = (event) => {
     setValue(event.target.value);
   };
-  console.log(list);
+  const clearList = ()=> {
+    setList([]);
+  }
+
   return (
     <>
+      <h1>Shopping List</h1>
       <form>
         <input type='text' placeholder='add item' value={value} onChange={(event) => { handleChange(event) }} />
         <button type='button' onClick={addItem}>Add Item</button>
+        <button type='button' onClick={clearList}>Clear</button>
       </form>
       <ul>
         {list.map((item, index) =>
